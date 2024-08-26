3b48c8dbf998eca0708507ce3548058ba64d7b58
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-08-26 07:14:20 +0000

    finised home page templating

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module personal_blog
 
 go 1.23.0
+
+require github.com/a-h/templ v0.2.771
--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"personal_blog/templates"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
+	})
+
+	mux.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
+		componant := templates.Home()
+		componant.Render(context.Background(), w)
+	})
+
+	mux.HandleFunc("GET /article/{id}", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	mux.HandleFunc("GET /admin", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	mux.HandleFunc("GET /edit/{id}", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	mux.HandleFunc("GET /new", func(w http.ResponseWriter, r *http.Request) {
 	})
 
 	server := http.Server{
